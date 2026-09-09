d5edd4d8b43a0d019810082858448a8eef183622
rrowlands <rrowlands@terraframe.com>
2026-09-09 16:12:21 -0600

    Fix HTTPS issue with proxy

--- src/build/webapp/entrypoint.sh
+++ src/build/webapp/entrypoint.sh
@@ -4,13 +4,33 @@ set -eu
 SERVER_XML="$CATALINA_HOME/conf/server.xml"
 
 if ! grep -q "RemoteIpValve" "$SERVER_XML"; then
-  sed -i '/<Host /a\
-        <Valve className="org.apache.catalina.valves.RemoteIpValve" \
-               remoteIpHeader="x-forwarded-for" \
-               protocolHeader="x-forwarded-proto" \
-               protocolHeaderHttpsValue="https" \
-               portHeader="x-forwarded-port" />' \
-    "$SERVER_XML"
+
+  awk '
+    BEGIN {
+      in_host = 0
+      inserted = 0
+    }
+
+    /<Host[[:space:]>]/ {
+      in_host = 1
+    }
+
+    {
+      print
+    }
+
+    in_host && />/ && !inserted {
+      print "        <Valve className=\"org.apache.catalina.valves.RemoteIpValve\""
+      print "               remoteIpHeader=\"x-forwarded-for\""
+      print "               protocolHeader=\"x-forwarded-proto\""
+      print "               protocolHeaderHttpsValue=\"https\""
+      print "               portHeader=\"x-forwarded-port\" />"
+      inserted = 1
+      in_host = 0
+    }
+  ' "$SERVER_XML" > "$SERVER_XML.tmp"
+
+  mv "$SERVER_XML.tmp" "$SERVER_XML"
 fi
 
 exec "$CATALINA_HOME/bin/catalina.sh" run
