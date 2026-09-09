1438d77afbd7ee79526dae7ea85e24c324e30823
rrowlands <rrowlands@terraframe.com>
2026-09-09 17:08:18 -0600

    Revert experimental SSL fixes.

--- src/build/webapp/entrypoint.sh
+++ src/build/webapp/entrypoint.sh
@@ -1,37 +1,6 @@
 #!/bin/sh
 set -eu
 
-SERVER_XML="$CATALINA_HOME/conf/server.xml"
-
-if ! grep -q "RemoteIpValve" "$SERVER_XML"; then
-
-  awk '
-    BEGIN {
-      in_host = 0
-      inserted = 0
-    }
-
-    /<Host[[:space:]>]/ {
-      in_host = 1
-    }
-
-    {
-      print
-    }
-
-    in_host && />/ && !inserted {
-      print "        <Valve className=\"org.apache.catalina.valves.RemoteIpValve\""
-      print "               internalProxies=\".*\""
-      print "               remoteIpHeader=\"x-forwarded-for\""
-      print "               protocolHeader=\"x-forwarded-proto\""
-      print "               protocolHeaderHttpsValue=\"http\""
-      print "               httpsServerPort=\"443\" />"
-      inserted = 1
-      in_host = 0
-    }
-  ' "$SERVER_XML" > "$SERVER_XML.tmp"
-
-  mv "$SERVER_XML.tmp" "$SERVER_XML"
-fi
+# Custom logic here, if needed.
 
 exec "$CATALINA_HOME/bin/catalina.sh" run
