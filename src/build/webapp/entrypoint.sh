0e20cef14e4085cc15cb22806ebc940a2738a416
rrowlands <rrowlands@terraframe.com>
2026-09-14 10:56:24 -0600

    Honor X-Forwarded-Proto header

--- src/build/webapp/entrypoint.sh
+++ src/build/webapp/entrypoint.sh
@@ -3,6 +3,36 @@ set -eu
 
 SERVER_XML="$CATALINA_HOME/conf/server.xml"
 
+# Configure Tomcat to honor X-Forwarded-* headers.
+if ! grep -q "RemoteIpValve" "$SERVER_XML"; then
+  awk '
+    BEGIN {
+      in_host = 0
+      inserted = 0
+    }
+
+    /<Host[[:space:]>]/ {
+      in_host = 1
+    }
+
+    {
+      print
+    }
+
+    in_host && />/ && !inserted {
+      print "        <Valve className=\"org.apache.catalina.valves.RemoteIpValve\""
+      print "               remoteIpHeader=\"x-forwarded-for\""
+      print "               protocolHeader=\"x-forwarded-proto\""
+      print "               protocolHeaderHttpsValue=\"https\""
+      print "               portHeader=\"x-forwarded-port\" />"
+      inserted = 1
+      in_host = 0
+    }
+  ' "$SERVER_XML" > "$SERVER_XML.tmp"
+
+  mv "$SERVER_XML.tmp" "$SERVER_XML"
+fi
+
 # --- TEMPORARY DIAGNOSTIC: log Host / X-Forwarded-* on every request ------
 # Appends the raw Host header plus X-Forwarded-Host, X-Forwarded-Proto, and
 # X-Forwarded-Port to the AccessLogValve pattern so we can see exactly what
